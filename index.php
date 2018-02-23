b4ca9db0cdcd0c1ea9be21527f72b6d453b6768d
tclauson2014 <tclauson2014@gmail.com>
2018-02-22 22:12:32 -0500

    Site version 1.0

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>tylerclauson.com | Home Page</title>
+  <link rel="stylesheet" href="/assets/css/home.css">
+</head>
+<body>
+  
+</body>
+</html>
